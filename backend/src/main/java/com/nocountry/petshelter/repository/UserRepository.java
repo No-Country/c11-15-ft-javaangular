7803c71dafe2736463eabc1f266fcf6eb8c6c71e
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-05-15 10:30:25 -0400

    Adding dependencies and application.properties

--- backend/src/main/java/com/nocountry/petshelter/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nocountry.petshelter.repository;
-
-import com.nocountry.petshelter.model.account.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
