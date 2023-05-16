798b517e88af4877dfb0b0c3e0ec1f59555e04ee
jhonpulido1990 <3825@holbertonstudents.com>
2023-05-16 08:26:18 -0500

    [add] = (login basic)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full',
+    title: 'login'
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    pathMatch: 'full',
+    title: 'register user'
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+    pathMatch: 'full',
+    title: 'Not Found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
