4a275e424f9bf582f2643d7405aef69c33f1d538
jhonpulido1990 <3825@holbertonstudents.com>
2023-05-17 02:10:46 -0500

    [add]= (add funtion navigate, login and register)

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  public register!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.register = this.formBuilder.group({
+      fullname:[''],
+      email:[''],
+      password:['']
+    })
+  }
+
+  registerUser(){
+    this.http.post<any>("http://localhost:5000/registerUser", this.register.value)
+    .subscribe(res => {
+      alert("registrado exitosamente");
+      this.register.reset();
+      this.router.navigate(['login']);
+    },err=>{
+      alert("ocurrio un problema")
+    })
+  }
 }
