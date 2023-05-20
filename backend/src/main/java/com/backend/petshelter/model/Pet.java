f15ba2a23a0498fb52d956533ed31e189eb8911a
Diego Villan <dnvx81@outlook.com>
2023-05-20 15:34:13 -0300

    Se agrega clase Pet y funcionalidades

--- backend/src/main/java/com/backend/petshelter/model/Pet.java
+++ backend/src/main/java/com/backend/petshelter/model/Pet.java
@@ -1,4 +1,36 @@
 package com.backend.petshelter.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter@Setter
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String nombre;
+    private String foto;
+    private String caracteristicas;
+    private String cuidados;
+    @NotBlank
+    private String contacto;
+
+    private Boolean activo = true;
+
+
+    public void borrar(){
+        this.activo = false;
+    }
+
 }
--- backend/src/main/java/com/backend/petshelter/service/PetService.java
+++
@@ -1,4 +0,0 @@
-package com.backend.petshelter.service;
-
-public class PetService {
-}
